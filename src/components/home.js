a35af50b29e835fb5dfb77223a380e3df18cfb58
lmitchell524 <lmitchell524@gmail.com>
2018-01-19 15:26:42 -0800

    routing complete

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => {
+    return(
+        <div>
+            <h1 className='center-align'>Welcome to Movie Quotes</h1>
+            <p>Lorem ipsum dolar sit amet asdf ,elasd</p>
+        </div>
+    )
+}
---
+++ src/components/movie_quotes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieQuotes = props => {
+    return(
+        <div>
+            <h1 className="center-align">Movie Quote</h1>
+            <h2>I'll be back!</h2>
+        </div>
+    )
+}
+
+export default MovieQuotes;
