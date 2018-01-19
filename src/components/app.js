a010c331ef63ff91e68cf2d98017eb9fb1c3b682
lmitchell524 <lmitchell524@gmail.com>
2018-01-19 12:14:25 -0800

    initial-commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,9 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
-import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+
 
 const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
-    </div>
+    <h1 className='center-align'>Booya</h1>
 );
 
 export default App;
